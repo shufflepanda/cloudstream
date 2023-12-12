4c681ee42570629424e923931556db532f8147bf
Eddy976 <47984460+Eddy976@users.noreply.github.com>
2023-12-12 19:44:29 +0100

    Update FrenchStreamProvider.kt

--- FrenchStreamProvider/src/main/kotlin/com/lagradost/FrenchStreamProvider.kt
+++ FrenchStreamProvider/src/main/kotlin/com/lagradost/FrenchStreamProvider.kt
@@ -10,7 +10,7 @@ import org.jsoup.nodes.Element
 
 
 class FrenchStreamProvider : MainAPI() {
-    override var mainUrl = "https://www.frenchstream.vip" //re ou ac ou city
+    override var mainUrl = "https://french-stream.digital/" //re ou ac ou city
     override var name = "FrenchStream"
     override val hasQuickSearch = false
     override val hasMainPage = true
